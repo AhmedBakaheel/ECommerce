fdaa8ec931476b7721ffee2ef792d39342ea81e1
AhmedBakaheel <ahmedomerbakaheel@gmail.com>
2025-08-05 12:14:57 +0300

    Create Repository

---
+++ ECommerce.Persistence/Repositories/ProductRepository .cs
@@ -0,0 +1,64 @@
+﻿using ECommerce.Domain.Entities;
+using ECommerce.Domain.Interfaces;
+using ECommerce.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Persistence.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Product entity)
+        {
+            await _context.Products.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _context.Products.Include(p => p.Category).ToListAsync();
+        }
+
+        public async Task<Product?> GetByIdAsync(int id)
+        {
+            return await _context.Products.Include(p => p.Category).FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task UpdateAsync(Product entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByCategoryAsync(int categoryId)
+        {
+            return await _context.Products.Where(p => p.CategoryId == categoryId).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsInStockAsync()
+        {
+            return await _context.Products.Where(p => p.Stock > 0).ToListAsync();
+        }
+    }
+}
