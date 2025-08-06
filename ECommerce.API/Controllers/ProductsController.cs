6568cdd1ee79b75ee2d50102822b6cffca19e564
AhmedBakaheel <ahmedomerbakaheel@gmail.com>
2025-08-06 09:51:19 +0300

    Create Products Controller

---
+++ ECommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using ECommerce.Application.DTOs.Products;
+using ECommerce.Application.Features.Products.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var query = new GetAllProductsQuery();
+            var products = await _mediator.Send(query);
+            return Ok(products);
+        }
+    }
+}
--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -1,23 +1,35 @@
+using ECommerce.Application.Mappings;
+using ECommerce.Domain.UnitOfWork;
+using ECommerce.Persistence.Data;
+using ECommerce.Persistence.UnitOfWork;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddMediatR(typeof(MappingProfiles).Assembly);
+
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
